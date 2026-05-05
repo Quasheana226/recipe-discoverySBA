e89980a7756a4d318c6192018ff8d3c767466cfe
Firee <newowner@News-MacBook-Pro.local>
2026-05-04 21:02:46 -0400

    set up React root rendering with StrictMode

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+ 
+
+
+
+//Mount React app into the #root 
+ReactDom.createRoot(document.getElementById('root')!).render(
+ 
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
